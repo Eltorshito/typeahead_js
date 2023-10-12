7cbf1d21dd33412c12ec853fb4849f930fa2068b
Eltorshito <cedricg697@gmail.com>
2023-10-12 13:13:59 +0200

    Update html and script

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
 const citiesData = [];
 
 const searchInput = document.querySelector('.search');
 
-const suggestionList = document.querySelector('.suggestion');
+const suggestions = document.querySelector('.suggestions');
 
 fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
 .then(response => response.json())
-.then(data => {
-    citiesData = data;
-});
+.then(data =>
+    citiesData.push(...data)
+);
 
 function findMatches(searchTerm, citiesArray) {
-    const regex = new RegExp(this.value, "gi");
+    const regex = new RegExp(`^${searchTerm}`, "gi"); //Changer le this.value //
     const matches = citiesArray.filter(place => {
         return place.city.match(regex) || place.state.match(regex);
     });
@@ -24,11 +26,18 @@ function numberWithCommas (x) {
 
 function displayMatches() {
     const matches = findMatches(searchInput.value, citiesData);
+    matches.sort((a, b) => a.city.localeCompare(b.city)); // Ajout //
     const html = matches.map(place => {
-        const regex = new regex(this.value, "gi");
-        const cityName = place.city.replace(regex, `<span class="hl"></span>`);
-        const stateName = place.state.replace(regex, `<span class="hl"></span>`);
-        return `<li>${cityName}, ${stateName}</li>`
+        const regex = new RegExp(this.value, "gi");
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `<li>${cityName}, ${stateName}, ${numberWithCommas(place.population)} </li>` // Ajout Population
     }).join('');
-
+    suggestions.innerHTML = html;
+    
 }
+
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
